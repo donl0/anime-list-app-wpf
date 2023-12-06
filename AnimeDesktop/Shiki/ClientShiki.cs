73c584dfac4ee5c4e29266d2a84d522c190833fd
donl0 <donl_117n@mail.ru>
2023-12-06 21:54:29 +0300

    Add singlton for shiki api

---
+++ AnimeDesktop/Shiki/ClientShiki.cs
@@ -0,0 +1,31 @@
+﻿using ShikimoriSharp;
+using ShikimoriSharp.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.Shiki
+{
+    class ClientShiki
+    {
+        const string ClientName = "Anime Burger";
+        const string ClientID = "ZEv-jFeYK_wdHhI8KY86_OcfKma66hy80aQ91MOICs4";
+        const string ClientSecret = "Im_G_ndXFnnMtzIBKydYW9b3UsRSZU7Lfa6oUmzyzKI";
+
+        private static ShikimoriClient? Instance;
+
+        public static void Init() {
+            if (Instance == null)
+            {
+                Instance = new ShikimoriClient(null, new ClientSettings(ClientName, ClientID, ClientSecret));
+                return;
+            }
+
+           throw new InvalidOperationException("Singleton instance is already created.");
+        }
+
+
+    }
+}
