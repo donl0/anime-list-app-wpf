84e69a1e7eefd0347119d6219641bc29411f1dfe
donl0 <donl_117n@mail.ru>
2023-12-08 01:08:52 +0300

    Move anime List in control

---
+++ AnimeDesktop/View/AnimeListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AnimeDesktop.View
+{
+    /// <summary>
+    /// Interaction logic for AnimeListControl.xaml
+    /// </summary>
+    public partial class AnimeListControl : UserControl
+    {
+        public AnimeListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
