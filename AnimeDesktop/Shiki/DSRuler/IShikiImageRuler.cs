99d9cf9c344483ee263f5c67dee50c6df5efc803
donl0 <donl_117n@mail.ru>
2023-12-08 01:04:54 +0300

    Add rule to change image in response from api

---
+++ AnimeDesktop/Shiki/DSRuler/IShikiImageRuler.cs
@@ -0,0 +1,9 @@
+﻿using ShikimoriSharp.Classes;
+
+namespace AnimeDesktop.Shiki
+{
+    internal interface IShikiImageRuler
+    {
+        public void Rule(List<Anime> animes);
+    }
+}
---
+++ AnimeDesktop/Shiki/DSRuler/ShikiImageRuler.cs
@@ -0,0 +1,17 @@
+﻿using ShikimoriSharp.Classes;
+
+namespace AnimeDesktop.Shiki
+{
+    class ShikiImageRuler : IShikiImageRuler
+    {
+        public void Rule(List<Anime> animes)
+        {
+            foreach (Anime anime in animes)
+            {
+                const string SHIKIURL = "https://shikimori.one";
+
+                anime.Image.Preview = SHIKIURL + anime.Image.Preview;
+            }
+        }
+    }
+}
