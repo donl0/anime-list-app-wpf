ddb41b586b0b3df8e52860555e47267af9f786fe
donl0 <donl_117n@mail.ru>
2023-12-08 01:06:56 +0300

    Add model with getting top 100 anime

---
+++ AnimeDesktop/Model/IAnimeListModel.cs
@@ -0,0 +1,9 @@
+﻿using ShikimoriSharp.Classes;
+
+namespace AnimeDesktop.Model
+{
+    internal interface IAnimeListModel
+    {
+        public Task<List<Anime>> TakeAnimes();
+    }
+}
---
+++ AnimeDesktop/Model/TopHundredModel.cs
@@ -0,0 +1,26 @@
+﻿using AnimeDesktop.Shiki;
+using ShikimoriSharp.Classes;
+using ShikimoriSharp.Settings;
+
+namespace AnimeDesktop.Model
+{
+    internal class TopHundredModel : IAnimeListModel
+    {
+        public async Task<List<Anime>> TakeAnimes()
+        {
+            var client = ClientShiki.Instance;
+
+            var search = await client.Animes.GetAnime(new AnimeRequestSettings
+            {
+                order = ShikimoriSharp.Enums.Order.ranked,
+                limit = 100
+
+            });
+
+            var anime = search;
+            List<Anime> anime1 = anime.ToList();
+
+            return anime1;
+        }
+    }
+}
