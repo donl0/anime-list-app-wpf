26639b97a210d3ee243e783c113637bb3e360f7d
donl0 <donl_117n@mail.ru>
2023-12-30 15:12:07 +0300

    Connect open certain anime logic to anime list

--- AnimeDesktop/ViewModel/BaseAnimeListViewModel.cs
+++ AnimeDesktop/ViewModel/BaseAnimeListViewModel.cs
@@ -1,6 +1,8 @@
-﻿using AnimeDesktop.Model;
+﻿using AnimeDesktop.Commands;
+using AnimeDesktop.Model;
 using AnimeDesktop.Shiki;
 using ShikimoriSharp.Classes;
+using System.Windows.Input;
 
 namespace AnimeDesktop.ViewModel
 {
@@ -9,9 +11,11 @@ namespace AnimeDesktop.ViewModel
         private readonly IShikiImageRuler _imageRuler;
 
         public NotifyTaskCompletion<List<Anime>> Values { get; private set; }
-
+        public ICommand OpenAnime { get; }
 
         public BaseAnimeListViewModel(IAnimeListModel startModel) {
+            OpenAnime = new OpenAnimeWindowCommand();
+
             _imageRuler = new ShikiImageRuler();
 
             Action<List<Anime>> onTaskCompleted = (animes) => _imageRuler.Rule(animes);
