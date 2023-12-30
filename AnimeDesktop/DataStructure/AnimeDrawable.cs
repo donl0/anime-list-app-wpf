b8c224f189164a86837f8e0b2a9c9be015a73a2a
donl0 <donl_117n@mail.ru>
2023-12-30 15:09:19 +0300

    Add functionality to remake api respons to sutable data for view

---
+++ AnimeDesktop/DataStructure/AnimeDrawable.cs
@@ -0,0 +1,29 @@
+﻿using ShikimoriSharp.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.DataStructure
+{
+    public class AnimeDrawable
+    {
+        public string Name { get; }
+        public string Genres { get; }
+        public string Description { get; }
+        public string Rating { get; }
+        public string Image { get; }
+        public long Episodes { get; }
+
+        public AnimeDrawable(string name, string genres, string description, string image, long episodes, string rating)
+        {
+            Name = name;
+            Genres = genres;
+            Description = description;
+            Image = image;
+            Episodes = episodes;
+            Rating = rating;
+        }
+    }
+}
---
+++ AnimeDesktop/Extensions/DrawableMaker.cs
@@ -0,0 +1,41 @@
+﻿using AnimeDesktop.DataStructure;
+using AnimeDesktop.Model;
+using ShikimoriSharp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.Extensions
+{
+    public static class DrawableMaker
+    {
+        public static async Task<AnimeDrawable> ToDrawable(this Anime anime) {
+            IAnimeIDModel animeIDModel = new AnimeIDModel();
+
+            AnimeID animeID = await animeIDModel.TakeAnime(anime.Id);
+
+            AnimeDrawable drawable = new AnimeDrawable(animeID.Name, ToGenresString(animeID.Genres), animeID.Description, anime.Image.Preview, animeID.Episodes, animeID.Score);
+
+            return drawable;
+        }
+
+        private static string ToGenresString(Genre[] genres) {
+            string genresString = "";
+            string splitter = " ,";
+
+            for (int i = 0; i < genres.Length; i++)
+            {
+                genresString += genres[i].Name;
+
+                if (i < genres.Length - 1)
+                {
+                    genresString += splitter;
+                }
+            }
+
+            return genresString;
+        }
+    }
+}
