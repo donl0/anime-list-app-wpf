9640d95456c3112b0f4065c0ebe5e554fc758e80
donl0 <donl_117n@mail.ru>
2023-12-30 15:07:58 +0300

    Add model for view full anime info

---
+++ AnimeDesktop/Model/AnimeIDModel.cs
@@ -0,0 +1,22 @@
+﻿using AnimeDesktop.Shiki;
+using ShikimoriSharp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.Model
+{
+    class AnimeIDModel : IAnimeIDModel
+    {
+        public async Task<AnimeID> TakeAnime(long id)
+        {
+            var client = ClientShiki.Instance;
+
+            var anime = await client.Animes.GetAnime(id);
+
+            return anime;
+        }
+    }
+}
---
+++ AnimeDesktop/Model/IAnimeIDModel.cs
@@ -0,0 +1,14 @@
+﻿using ShikimoriSharp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.Model
+{
+    internal interface IAnimeIDModel
+    {
+        public Task<AnimeID> TakeAnime(long id);
+    }
+}
