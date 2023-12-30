07e715bc8717caad66dee313e27ab2137c72568e
donl0 <donl_117n@mail.ru>
2023-12-30 15:09:54 +0300

    Add command that open Anime Window

---
+++ AnimeDesktop/Commands/OpenAnimeWindowCommand.cs
@@ -0,0 +1,27 @@
+﻿using AnimeDesktop.View;
+using ShikimoriSharp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.Commands
+{
+    class OpenAnimeWindowCommand : BaseCommand
+    {
+        public override bool CanExecute(object? parameter)
+        {
+            return parameter is Anime;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Anime selectedItem = parameter as Anime;
+
+            CertainAnimeView animeView = new CertainAnimeView(selectedItem);
+
+            animeView.Show();
+        }
+    }
+}
