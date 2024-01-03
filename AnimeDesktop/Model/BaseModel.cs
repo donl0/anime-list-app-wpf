68eb727840891c33af7d85a202156ac75ec33527
donl0 <donl_117n@mail.ru>
2024-01-03 21:49:45 +0300

    Add base model

---
+++ AnimeDesktop/Model/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using AnimeDesktop.Shiki;
+
+namespace AnimeDesktop.Model
+{
+    public abstract class BaseModel<DS, TClientInstance, TClientType> : IModel<DS> where TClientInstance : IClient<TClientType>
+    {
+        protected TClientInstance Client;
+
+        public BaseModel(TClientInstance client) {
+            Client = client;
+        }
+
+        public abstract Task<DS> TakeData();
+    }
+}
---
+++ AnimeDesktop/Model/IPayloadedModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeDesktop.Model
+{
+    internal interface IPayloadedModel<DS, Payload>
+    {
+        public Task<DS> TakeData(Payload payload);
+    }
+}
