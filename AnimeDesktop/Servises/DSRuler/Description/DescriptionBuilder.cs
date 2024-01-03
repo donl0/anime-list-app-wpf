03f28526e6c6674c3bf64387eaf8ad86dfd360e9
donl0 <donl_117n@mail.ru>
2024-01-03 23:11:16 +0300

    Extract interface Description ruler

---
+++ AnimeDesktop/Servises/DSRuler/Description/DescriptionBuilder.cs
@@ -0,0 +1,51 @@
+﻿using AnimeDesktop.Extensions;
+
+namespace AnimeDesktop.Servises.DSRuler.Description
+{
+    internal class DescriptionBuilder : IDescriptionBuilder
+    {
+        private string _ruleObject;
+
+        public DescriptionBuilder(string ruleObject)
+        {
+            _ruleObject = ruleObject;
+        }
+
+        public DescriptionBuilder WithCharacter()
+        {
+            _ruleObject = _ruleObject.RemoveString(@"\[character=\d+\]");
+            _ruleObject = _ruleObject.RemoveString(@"\[/character]");
+
+            return this;
+        }
+
+        public DescriptionBuilder WithAnime()
+        {
+            _ruleObject = _ruleObject.RemoveString(@"\[anime=\d+\]");
+            _ruleObject = _ruleObject.RemoveString(@"\[/anime]");
+
+            return this;
+        }
+
+        public DescriptionBuilder WithITag()
+        {
+            _ruleObject = _ruleObject.RemoveString(@"\[i]");
+            _ruleObject = _ruleObject.RemoveString(@"\[/i]");
+
+            return this;
+        }
+
+        public DescriptionBuilder WithPerson()
+        {
+            _ruleObject = _ruleObject.RemoveString(@"\[person=\d+\]");
+            _ruleObject = _ruleObject.RemoveString(@"\[/person]");
+
+            return this;
+        }
+
+        public string Builed()
+        {
+            return _ruleObject;
+        }
+    }
+}
---
+++ AnimeDesktop/Servises/DSRuler/Description/IDescriptionBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimeDesktop.Servises.DSRuler.Description
+{
+    internal interface IDescriptionBuilder
+    {
+        string Builed();
+        public DescriptionBuilder WithAnime();
+        public DescriptionBuilder WithCharacter();
+        public DescriptionBuilder WithITag();
+        public DescriptionBuilder WithPerson();
+    }
+}
