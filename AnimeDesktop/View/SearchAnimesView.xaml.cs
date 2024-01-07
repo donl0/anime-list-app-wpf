57f97f0c68cf5194f01b84bd748643f19acefa44
donl0 <donl_117n@mail.ru>
2024-01-07 19:32:35 +0300

    Add search anime view

---
+++ AnimeDesktop/View/SearchAnimesView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AnimeDesktop.View
+{
+    public partial class SearchAnimesView : UserControl
+    {
+        public SearchAnimesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
