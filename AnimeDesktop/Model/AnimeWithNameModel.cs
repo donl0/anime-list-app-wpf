9df276666b3b1463ebf1208012d596a7b55ad52b
donl0 <donl_117n@mail.ru>
2024-01-07 19:33:25 +0300

    Add model that try to find animes with name

---
+++ AnimeDesktop/Model/AnimeWithNameModel.cs
@@ -0,0 +1,29 @@
+﻿using AnimeDesktop.Shiki;
+using ShikimoriSharp.Classes;
+using ShikimoriSharp;
+using ShikimoriSharp.Settings;
+
+namespace AnimeDesktop.Model
+{
+    public class AnimeWithNameModel : BasePayloadedModel<List<Anime>, ClientShiki, ShikimoriClient, string>
+    {
+        public AnimeWithNameModel(ClientShiki client) : base(client)
+        {
+        }
+
+        public async override Task<List<Anime>> TakeData(string value)
+        {
+            var clientInstance = Client.Instance;
+
+            var search = await clientInstance.Animes.GetAnime(new AnimeRequestSettings
+            {
+                search = value,
+                limit = 100
+            });
+
+            List<Anime> anime = search.ToList();
+
+            return anime;
+        }
+    }
+}
