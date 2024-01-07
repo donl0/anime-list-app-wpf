67a66ec2874aff6ba7c560d2acdf6b9f5527b221
donl0 <donl_117n@mail.ru>
2024-01-07 19:34:08 +0300

    Add command that set search text to search Anime viewModel and navigate

---
+++ AnimeDesktop/Commands/SearchAnimeCommand.cs
@@ -0,0 +1,28 @@
+﻿using AnimeDesktop.Model;
+using AnimeDesktop.Servises;
+using AnimeDesktop.ViewModel;
+
+namespace AnimeDesktop.Commands
+{
+    public class SearchAnimeCommand<TVM> : BaseCommand where TVM: BaseAnimeListViewModel
+    {
+        private readonly NavigationService<TVM> _navigationService;
+        private readonly ISearchModel _searchModel;
+
+        public SearchAnimeCommand(ISearchModel searchModel, NavigationService<TVM> navigationService)
+        {
+
+            _searchModel = searchModel;
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            string value = (string)parameter;
+
+            _navigationService.Navigate();
+
+            _searchModel.SetSearchText(value);
+        }
+    }
+}
