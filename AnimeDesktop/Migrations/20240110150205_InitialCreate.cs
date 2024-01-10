5c86863f1540862395851ae5bbc37008028ca7ae
donl0 <donl_117n@mail.ru>
2024-01-10 18:28:03 +0300

    Initial commit

---
+++ AnimeDesktop/Migrations/20240110150205_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AnimeDesktop.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AbandonedAnime",
+                columns: table => new
+                {
+                    AnimeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbandonedAnime", x => x.AnimeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Anime",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Anime", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlannedAnime",
+                columns: table => new
+                {
+                    AnimeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlannedAnime", x => x.AnimeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRating",
+                columns: table => new
+                {
+                    AnimeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRating", x => x.AnimeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchedAnime",
+                columns: table => new
+                {
+                    AnimeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchedAnime", x => x.AnimeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbandonedAnime");
+
+            migrationBuilder.DropTable(
+                name: "Anime");
+
+            migrationBuilder.DropTable(
+                name: "PlannedAnime");
+
+            migrationBuilder.DropTable(
+                name: "UserRating");
+
+            migrationBuilder.DropTable(
+                name: "WatchedAnime");
+        }
+    }
+}
