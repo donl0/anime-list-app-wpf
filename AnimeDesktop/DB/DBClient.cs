2723943420600b4a54334bd662411a9e10f68fad
donl0 <donl_117n@mail.ru>
2024-01-14 01:10:52 +0300

    Add DataBase client and move connection info to secret

---
+++ AnimeDesktop/DB/DBClient.cs
@@ -0,0 +1,30 @@
+﻿using AnimeDesktop.Base;
+using AnimeDesktop.DB.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AnimeDesktop.DB
+{
+    class DBClient: DbContext, IClient<DbContext>
+    {
+        public DbContext Instance => this;
+
+        public DbSet<Anime> Anime { get; set; }
+        public DbSet<UserRating> UserRating { get; set; }
+        public DbSet<WatchedAnime> WatchedAnime { get; set;}
+        public DbSet<PlannedAnime> PlannedAnime { get; set;}
+        public DbSet<AbandonedAnime> AbandonedAnime { get;set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("DB/appsecrets.json")
+                .Build();
+
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
