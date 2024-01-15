fa88040182f50f9395af0a71f315199f9c3d8afc
donl0 <donl_117n@mail.ru>
2024-01-15 03:54:35 +0300

    add logic to image ruler

--- AnimeDesktop/Servises/DSRuler/ShikiImageRuler.cs
+++ AnimeDesktop/Servises/DSRuler/ShikiImageRuler.cs
@@ -9,7 +9,9 @@ namespace AnimeDesktop.Servises.DSRuler
             foreach (Anime anime in animes)
             {
                 const string SHIKIURL = "https://shikimori.one";
-
+                anime.Image.Original = SHIKIURL + anime.Image.Original;
+                anime.Image.X96 = SHIKIURL + anime.Image.X96;
+                anime.Image.X48 = SHIKIURL + anime.Image.X48;
                 anime.Image.Preview = SHIKIURL + anime.Image.Preview;
             }
         }
