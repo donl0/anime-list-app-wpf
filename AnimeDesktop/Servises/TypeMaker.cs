9aba04f95a1ce294ef8f32c33b8636f8c38aab8c
donl0 <donl_117n@mail.ru>
2024-01-15 03:54:15 +0300

    Add type convertor from animeID to anime

---
+++ AnimeDesktop/Servises/TypeMaker.cs
@@ -0,0 +1,28 @@
+﻿using ShikimoriSharp.Classes;
+
+namespace AnimeDesktop.Servises
+{
+    public static class TypeMaker
+    {
+        public static AnimeID MakeAnimeType(this AnimeID animeID, out Anime anime)
+        {
+            anime = new Anime
+            {
+                Id = animeID.Id,
+                Name = animeID.Name,
+                Russian = animeID.Russian,
+                Image = animeID.Image,
+                Url = animeID.Url,
+                Kind = animeID.Kind,
+                Score = animeID.Score,
+                Status = animeID.Status,
+                AiredOn = animeID.AiredOn,
+                ReleasedOn = animeID.ReleasedOn,
+                Episodes = animeID.Episodes,
+                EpisodesAired = animeID.EpisodesAired
+            };
+
+            return animeID;
+        }
+    }
+}
